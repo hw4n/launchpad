21ea4379f28406224c3771cf33e2deeaedb5fcb4
Hwanhee Chae <hh@hwan.me>
2020-11-03 22:25:14 +0900

    move / route to routes/index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.render('form');
-})
-
 import apiRoute from "./routes"
 app.use("/", apiRoute);
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,11 @@ import express from "express";
 const router = express.Router();
 import helper from "../helper/links";
 
+router.route("/")
+  .get((req, res) => {
+    res.render('form');
+  })
+
 router.route("/:source")
   .get(helper.followLink)
 
